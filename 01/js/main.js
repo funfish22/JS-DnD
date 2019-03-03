aa50a0a2cebf4135449b7069d398926f788dc930
funfish22 <j26391442@gmail.com>
2019-03-03 22:11:38 +0800

    first commit

---
+++ 01/js/main.js
@@ -0,0 +1,62 @@
+// ;(function(){
+//     var inner = document.querySelector('.inner')
+//     // var bTitle = ''
+//     var bTitle = '<div class="col-4 mb-4"><div class="line"></div><h1 class="title">九九乘法表</h1><p class="subtitle">MULTIPLICATION CHART</p><div class="line"></div></div >'
+
+//     for(var i=2; i<10; i++){
+//         bTitle += '<div class="col-4 border mb-4"><div class="numInner"><h2 class="num__title">'+ i +'</h2>'
+        
+//         for(var j=1; j<10; j++){
+//             total =+ i + ' x ' + j +'＝' + i*j
+//             bTitle += '<p class="num__p">'+ i +' × '+ j +' ＝ '+ i*j +'</p>'
+
+//             // innerTotal.textContent = total
+//         }
+//         bTitle += '</div></div>'
+//         inner.innerHTML = bTitle
+//     }
+// }())
+
+
+;(function(){
+    var inner = document.querySelector('.inner')
+    // var bTitle = ''
+    var box = document.createElement('div')
+    box.setAttribute('class', 'col-4 mb-4')
+
+    inner.appendChild(box).innerHTML = 
+    `
+        <div class="line"></div>
+        <h1 class="title">九九乘法表</h1>
+        <p class="subtitle">MULTIPLICATION CHART</p>
+        <div class="line"></div>
+    `
+
+    for(var i=2; i<10; i++){
+        var ul = document.createElement('ul')
+        var li = document.createElement('li')
+        var box2 = document.createElement('div')
+        box2.setAttribute('class', 'col-4 border mb-4')
+        ul.setAttribute('class', 'numInner')
+        li.setAttribute('class', 'num__p')
+        li.textContent = i
+        ul.appendChild(li)
+        box2.appendChild(ul)
+        
+        for(var j=1; j<10; j++){
+            var li = document.createElement('li')
+            li.setAttribute('class', 'num__p')
+            li.textContent = `${i} × ${j} = ${i * j}`
+            ul.appendChild(li)
+        }
+        
+        inner.appendChild(box2)
+    }
+}())
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
---
+++ 02/js/main-dist.js
@@ -0,0 +1,33 @@
+'use strict';
+
+;(function () {
+    var hourHand = document.querySelector('.hourline');
+    var minHand = document.querySelector('.minline');
+    var secHand = document.querySelector('.secline');
+
+    function time() {
+        var time = new Date();
+        var hour = time.getHours();
+        var min = time.getMinutes();
+        var sec = time.getSeconds();
+        console.log(sec);
+
+        //時針度數  小時*360度 / 1圈12個小時  加上  已走分鐘的度數
+        var hourdeg = hour * 360 / 12 + min * 360 / (12 * 60);
+        //分針度數  分*360度 / 1圈60分鐘  加上  已走秒數的度數
+        var mindeg = min * 360 / 60 + sec * 360 / (60 * 60);
+        //秒針度數  秒*360度 / 1圈60秒
+        var secdeg = sec * 360 / 60;
+
+        return [hourdeg, mindeg, secdeg];
+    }
+    function set() {
+        var timedeg = time();
+        console.log(timedeg);
+        hourHand.style.transform = 'translateX(-50%) rotate(' + (-180 + timedeg[0]) + 'deg)';
+        minHand.style.transform = 'translateX(-50%) rotate(' + (180 + timedeg[1]) + 'deg)';
+        secHand.style.transform = 'translateX(-50%) rotate(' + (180 + timedeg[2]) + 'deg)';
+    }
+    // set()
+    setInterval(set, 1000);
+})();
